b184237ca98f03f04719f094c830dbbdaaa58b3d
elgharbi <elgharbiay@gmail.com>
2024-02-09 03:04:21 +0100

    Add password hashing and validation functions

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+function to hash password
+"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
