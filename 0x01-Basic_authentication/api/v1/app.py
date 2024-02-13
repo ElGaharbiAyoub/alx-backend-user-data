31937c02e64cf0825cb4b7935dbaea7bd66dce72
elgharbi <elgharbiay@gmail.com>
2024-02-13 20:13:46 +0100

    Fix pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,12 +13,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE  = os.getenv('AUTH_TYPE')
-if AUTH_TYPE  == 'auth':
+
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if AUTH_TYPE  == 'basic_auth':
+if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
