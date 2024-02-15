c8ba1cd834fd0530d3119fc2d9cc5a553d995f94
elgharbi <elgharbiay@gmail.com>
2024-02-15 16:45:26 +0100

    Update authentication requirements in before_request() function

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,10 +55,14 @@ def before_request() -> str:
     """ Before request handler
     """
     if auth is not None:
-        if auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
-            if auth.authorization_header(request) is None:
+        if auth.require_auth(
+                request.path,
+                ['/api/v1/status/',
+                    '/api/v1/unauthorized/',
+                    '/api/v1/forbidden/',
+                    '/api/v1/auth_session/login/']):
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             request.current_user = auth.current_user(request)
             if request.current_user is None:
