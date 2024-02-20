791fa81d575dff4e84edd5306151ba1adb5eac9c
elgharbi <elgharbiay@gmail.com>
2024-02-20 22:14:24 +0100

    Hash password

--- 0x03-user_authentication_service/0-main.py
+++ 0x03-user_authentication_service/0-main.py
@@ -64,26 +64,38 @@
 
 # =================== task 3 ====================
 
+# """
+# Main file
+# """
+# from db import DB
+# from user import User
+
+# from sqlalchemy.exc import InvalidRequestError
+# from sqlalchemy.orm.exc import NoResultFound
+
+
+# my_db = DB()
+
+# email = 'test@test.com'
+# hashed_password = "hashedPwd"
+
+# user = my_db.add_user(email, hashed_password)
+# print(user.id)
+
+# try:
+#     my_db.update_user(user.id, hashed_password='NewPwd')
+#     print("Password updated")
+# except ValueError:
+#     print("Error")
+
+
+# =================== task 4 ====================
+# ================ _hash_password  =======================
+
+#!/usr/bin/env python3
 """
 Main file
 """
-from db import DB
-from user import User
+from auth import _hash_password
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-
-my_db = DB()
-
-email = 'test@test.com'
-hashed_password = "hashedPwd"
-
-user = my_db.add_user(email, hashed_password)
-print(user.id)
-
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+print(_hash_password("Hello Holberton"))
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" Hash password module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ Hash a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
