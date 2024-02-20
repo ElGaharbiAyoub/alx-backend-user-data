4a9aba4c3edc7c0e9ecbc603038eaace4635c657
elgharbi <elgharbiay@gmail.com>
2024-02-20 22:49:21 +0100

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,6 +13,7 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def _generate_uuid() -> str:
     """ Generate a UUID
     """
