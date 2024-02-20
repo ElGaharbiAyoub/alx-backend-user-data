5585f4d8b09de98cd6abb3b0bedc9cc6df6f7b90
elgharbi <elgharbiay@gmail.com>
2024-02-20 22:10:07 +0100

    Fix bug in update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,6 +54,7 @@ class DB:
         """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
             setattr(user, key, value)
         self._session.commit()
-        return None
