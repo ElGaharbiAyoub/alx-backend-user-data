271c237cfd5c55edf363e81f5d618d1e927f68ff
elgharbi <elgharbiay@gmail.com>
2024-02-21 14:16:09 +0100

    Add login route to user authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,6 +32,22 @@ def register_user() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions
+    JSON body:
+      - email
+      - password
+    Return: session id
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            return jsonify({"email": email, "message": "logged in"})
+    return abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
