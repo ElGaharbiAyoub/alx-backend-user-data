fea5e39e8fa4e6e2834395ad0f33cec2cb5045b7
elgharbi <elgharbiay@gmail.com>
2024-02-21 15:32:36 +0100

    Fix typo in get_reset_password_token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,15 +82,15 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_rest_password_token(self, email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """get reset password token
         """
         try:
             user = self._db.find_user_by(email=email)
-            
+
         except NoResultFound:
             raise ValueError
 
-        resst_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=resst_token)
-        return resst_token
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
