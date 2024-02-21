88eaf4c673ab61f7fed4334d13c25a73b62cf5ad
elgharbi <elgharbiay@gmail.com>
2024-02-21 15:33:00 +0100

    Update reset token generation and user update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,10 +87,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError
-
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
