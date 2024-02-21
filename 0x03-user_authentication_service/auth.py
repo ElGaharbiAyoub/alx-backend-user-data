cb7f5b5a02454554e72ca1343decc976ffa5dd37
elgharbi <elgharbiay@gmail.com>
2024-02-21 14:51:22 +0100

    Add get_user_from_session_id method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 from db import DB
 import bcrypt
 import uuid
+from typing import Union, TypeVar
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -56,5 +57,18 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(
+            self,
+            session_id: str) -> Union[TypeVar('User'), None]:
+        """Get user from session id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
